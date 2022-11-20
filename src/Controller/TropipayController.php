eba42c7e0d0bbde12ee22add993c8cc65d9bb349
Alex900930 <aherreramilet@gmail.com>
2022-11-20 11:02:56 -0500

    montando api tropipay

--- src/Controller/TropipayController.php
+++ src/Controller/TropipayController.php
@@ -16,15 +16,95 @@
         #[Route('/met_tropipay/{met_pago}', name: 'app_met_tropipay')]
         public function pagotropipay(Request $request, string $met_pago): JsonResponse
         {
-            $name = $this->metod1();
+            $name = $this->getUrl();
             return new JsonResponse(['data'=>'Hola ' . $met_pago. ' '.$name]);
 
 
         }
 
-        private function metod1(){
-            return 'Pepe';
+        private function getToken(){
+
+            $curl = curl_init();
+      
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://tropipay-dev.herokuapp.com/api/access/login',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{
+            "email": "aherreramilet@gmail.com",
+            "password": "Harold*1845"
+            }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+            ));
+      
+            $response = curl_exec($curl);
+            $array = explode(",", $response);
+            $token1=explode(":", $array[0]);
+            $token2= str_replace('"', '', $token1[1]);
+            
+            
+            curl_close($curl);
+            
+      
+            return $token2;
+          }
+
+
+        private function getUrl(){
+
+            $token=$this->getToken();
+            $curl = curl_init();
+        
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://tropipay-dev.herokuapp.com/api/paymentcards',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{
+            "reference": "your-own-system-reference",
+            "concept": "test",
+            "description": "test",
+            "amount": 100000,
+            "currency": "EUR",
+            "singleUse": false,
+            "reasonId": 2,
+            "expirationDays": 1,
+            "lang": "es",
+            "urlSuccess": "https://mi-negocio.com/pago-ok",
+            "urlFailed": "https://mi-negocio.com/pago-ko",
+            "urlNotification": "https://mi-negocio.com/notificacion-pago"
+            }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer '.$token
+            ),
+            ));
+        
+            $response = curl_exec($curl);
+            
+            $array = explode(",", $response);
+            $array1=explode(":", $array[25]);
+            $arr=$array1[1].':'.$array1[2];
+            //print_r($arr);
+                        
+            curl_close($curl);
+            return $arr;
+        
         }
+
+        
+        
 
 
     }
